5c35e2e7e248e8989ae2bb2638476640414a3e51
Tiba3195 <justin_beast@hotmail.com>
2023-12-31 23:17:42 +1100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -7,6 +7,8 @@ LOCAL_MODULE_FILENAME := libgpiocontrol
 LOCAL_SRC_FILES := gpiocontrol.cpp
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+LOCAL_SYSTEM_MODULE := true
 LOCAL_CPP_FEATURES := rtti exceptions
 
 include $(BUILD_SHARED_LIBRARY)
